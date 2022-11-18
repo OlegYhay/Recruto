1a34f3a794d08240f0d979d0d49d4769f3d6f73c
Oleg <jadk.fre@Mail.ru>
2022-11-18 09:29:00 +0500

    initial

---
+++ apiV1/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HelloWorld(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'Hello.html'
+
+    def get(self, request, *args, **kwargs):
+        name = self.request.query_params.get("name", None)
+        message = self.request.query_params.get("message", None)
+        if name == None or message == None:
+            return HttpResponseNotFound('You must input name and message!')
+        return Response({'name': name, 'message': message})
